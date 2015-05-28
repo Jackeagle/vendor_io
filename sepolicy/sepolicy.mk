f598d95b2ae61c32f68b768842341c130bbab9af
Alberto97 <albertop2197@gmail.com>
2015-05-28 10:58:11 -0400

    Vendor : Add Sepolicy InfinitiveOS
    Credits : CyanogenMod , F-AOSP
    Originally Picked : https://github.com/F-AOSP/platform_vendor_aosp/commit/d3359a0263fe2a0020173f98a055139d8182d5a4
    
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

---
+++ sepolicy/sepolicy.mk
@@ -0,0 +1,22 @@
+#
+# This policy configuration will be used by all products that
+# Inherit from InfinitiveOS
+#
+
+BOARD_SEPOLICY_DIRS += \
+    vendor/io/sepolicy
+
+BOARD_SEPOLICY_UNION += \
+    file.te \
+    file_contexts \
+    genfs_contexts \
+    seapp_contexts \
+    service_contexts \
+    auditd.te \
+    installd.te \
+    netd.te \
+    sysinit.te \
+    system.te \
+    ueventd.te \
+    vold.te \
+    mac_permissions.xml
