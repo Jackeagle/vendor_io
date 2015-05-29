BloodofJesus/bobtipop-vendor

Backuptool Support
Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

config/common.mk
@@ -57,3 +57,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/optipop/prebuilt/lib/libjni_latinime.so:system/lib/libjni_latinime.so
 
+# Backuptool support
+PRODUCT_COPY_FILES += \
+    vendor/optipop/prebuilt/addon.d/50-optipop.sh:system/addon.d/50-optipop.sh \
+    vendor/optipop/prebuilt/bin/backuptool.functions:system/bin/backuptool.functions \
+    vendor/optipop/prebuilt/bin/backuptool.sh:system/bin/backuptool.sh

Jackeagle/vendor_io

Conflicts:
	config/common.mk

configs/common.mk
@@ -50,3 +50,9 @@ PRODUCT_COPY_FILES += \
 
 # Common overlays
 PRODUCT_PACKAGE_OVERLAYS += vendor/io/overlays/common
+
+# Backuptool support
+PRODUCT_COPY_FILES += \
+    vendor/io/prebuilts/addon.d/50-infinitive.sh:system/addon.d/50-infinitive.sh \
+    vendor/io/prebuilts/bin/backuptool.functions:system/bin/backuptool.functions \
+    vendor/io/prebuilts/bin/backuptool.sh:system/bin/backuptool.sh